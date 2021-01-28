dd1f66b1a87ab3d721d62860f166c4c3c79141a4
thza007 <mewsk134@hotmail.com>
2021-01-28 21:12:21 +0700

    Send Assignment Week1

---
+++ Assignment Week1/Assignment Week1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_Week1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (txt.Text == "Facebook" || txt.Text == "Google" || txt.Text =="Line" || txt.Text =="OnlineHD" || txt.Text == "Thanachot")
+            {
+                showTxt.Text = "[ Google ] is found.";
+            }
+            else
+            {
+                showTxt.Text = "[ Google ] is not found.";
+            }
+        }
+    }
+}
